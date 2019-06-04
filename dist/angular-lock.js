7621a96a04441f3d275f3c058f1b11fa86e1286b
Luciano Balmaceda <balmacedaluciano@gmail.com>
2019-06-04 11:15:34 -0300

    v3.0.2

--- dist/angular-lock.js
+++ dist/angular-lock.js
@@ -24,7 +24,7 @@
       this.options = config.options || {};
       this.options._telemetryInfo = {
         name: 'angular-lock',
-        version: '3.0.0'
+        version: '3.0.2'
       };
     };
 
